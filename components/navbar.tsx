668b05ed8450c2af862400d88e54c3586b68d861
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-09-06 16:22:03 +0530

    added navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import { FC } from "react";
+import { ModeToggle } from "./mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+import { MobileSidebar } from "./mobile-sidebar";
+
+const font = Poppins({ weight: "600", subsets: ["latin"] });
+interface navbarProps {}
+
+export const Navbar: FC<navbarProps> = ({}) => {
+  return (
+    <>
+      <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 h-16 border-b border-primary/10 bg-secondary">
+        <div className="flex items-center">
+          <MobileSidebar />
+          <Link href="/">
+            <h1
+              className={cn(
+                "hidden md:block text-xl md:text-3xl font-bold text-primary",
+                font.className
+              )}
+            >
+              SumRev
+            </h1>
+          </Link>
+        </div>
+        <div className="flex items-center gap-x-3">
+          {/* {!isPro && (
+          <Button onClick={proModal.onOpen} size="sm" variant="premium">
+            Upgrade
+            <Sparkles className="h-4 w-4 fill-white text-white ml-2" />
+          </Button>
+        )} */}
+          <ModeToggle />
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </div>
+    </>
+  );
+};
