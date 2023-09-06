d74fb17fa436ae291d8f002a6484ffd3dca61ec1
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-09-06 16:47:36 +0530

    added home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Navbar } from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+           <Navbar/>
             {children}
           </ThemeProvider>
         </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,66 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      SumRev with Postgres
+      <div className="xl:my-56 my-20 md:mt-48 mx-[10vw] lg:mx-[10vw] xl:mx-[15vw] flex flex-col md:flex-row items-center justify-between">
+        <div className="md:w-1/2">
+          <h1 className="text-5xl font-satoshiBlack">
+            SumRev - Reviews, Summaries and more
+            <br />
+            <h2 className="text-3xl mt-5">
+              Made Possible by{" "}
+              <a
+                href="https://passage.1password.com/"
+                target="_blank"
+                className="text-2xl md:text-3xl hover:underline font-satoshiBlack bg-gradient-to-r bg-clip-text text-transparent from-yellow-500 via-purple-500 to-red-500 animate-text"
+              >
+                Outerbase
+              </a>
+            </h2>
+          </h1>
+          <div className="md:hidden">
+            <Image
+              src="/images/meet.png"
+              alt="hero-image"
+              width={500}
+              height={500}
+              className="rounded-xl"
+              loading="lazy"
+            />
+          </div>
+          <h2 className="text-3xl font-satoshiBold mt-10">
+            Easily share and review books and products you love{" "}
+          </h2>
+          <h3 className="text-xl mt-5 font-ranadeRegular">
+            This is my submission for Outerbase X Hashnode hackathon.{" "}
+            <a
+              href="https://shreyas-chaliha.hashnode.dev/"
+              target="_blank"
+              className="underline hover:cursor-pointer text-base hover:text-indigo-500"
+            >
+              Hashnode Article
+            </a>
+          </h3>
+
+          <Link href={"/book"}>
+            <Button variant={"default"} className="mt-4">
+              Discover Books
+            </Button>
+          </Link>
+        </div>
+        <div className="collapse max-sm:w-0 max-sm:h-0 md:visible">
+          {/* <Image
+            src="/images/meet.png"
+            alt="hero-image"
+            width={500}
+            height={500}
+            className="rounded-xl"
+          /> */}
+        </div>
+      </div>
     </main>
   );
 }
