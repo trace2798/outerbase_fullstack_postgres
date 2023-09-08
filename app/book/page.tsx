d5c046328e28caa94336c259c10b962980ea5994
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-09-09 00:40:48 +0530

    slight change in home layout

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -25,7 +25,7 @@ const page: FC<pageProps> = async ({}) => {
   console.log(data, "DATA");
   return (
     <>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 pb-10">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-2 pb-10">
         {data.response.items.map((book: any, index: any) => (
           <Card key={index} className="max-w-sm">
             <Link href={`/book/${book.id}`}>
