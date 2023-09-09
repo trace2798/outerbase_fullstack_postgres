08cc6865da8143240d740834501b3fa90ba42b9f
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-09-09 22:11:53 +0530

    fixed summarie if issue

--- app/summary/edit/[id]/page.tsx
+++ app/summary/edit/[id]/page.tsx
@@ -11,31 +11,50 @@ interface pageProps {
 const page = async ({ params }: pageProps) => {
   const idInInt = Number(params.id);
   console.log(idInInt);
-  const summaries = await prismadb.summary.findUnique({
-    where: {
-      id: idInInt,
-    },
-  });
-  if (summaries === null) {
+  // const summaries = await prismadb.summary.findUnique({
+  //   where: {
+  //     id: idInInt,
+  //   },
+  // });
+  const summariesById = await fetch(
+    `https://middle-indigo.cmd.outerbase.io/getSummaryById?id=${idInInt}`,
+    {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+      },
+    }
+  );
+  console.log(summariesById, "OUTERBASE COMMAND Summary");
+
+  const summar = await summariesById.json();
+  console.log(summar, "SUMMAR");
+  if (summar.response.items.length === 0) {
     return (
       <>
-        <h1>404</h1>
+        <h1>
+          Oops. Looks like it is empty. Write a summary or review to get started
+        </h1>
       </>
     );
   }
-  console.log(summaries);
+  // console.log(summaries);
   return (
     <>
       <div className="mx-[10vw] flex justify-center h-screen items-center">
         <div className="w-full">
-          <SummaryFormEdit
-            id={idInInt}
-            title={summaries.title}
-            content={summaries?.content}
-            user_id={summaries.user_id}
-            user_name={summaries.user_name}
-            book_id={summaries.book_id}
-          />{" "}
+          {summar.response.items.map((summaries: any, index: any) => (
+            <div key={index}>
+              <SummaryFormEdit
+                id={idInInt}
+                title={summaries.title}
+                content={summaries?.content}
+                user_id={summaries.user_id}
+                user_name={summaries.user_name}
+                book_id={summaries.book_id}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </>
