737422c58debee80113f5cc4a63eb3e0fadc42d7
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-09-11 01:46:18 +0530

    re opened registration

--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -3,8 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <>
-      {/* <SignUp /> */}
-      <h1>Registration is currently closed. Sorry about the inconvenience.</h1>
+      <SignUp />
     </>
   );
 }
