b3e53881700fb97fb412b936daf47b1127bbd58f
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-09-19 23:48:19 +0530

    made changes in chat layout

--- app/ai/page.tsx
+++ app/ai/page.tsx
@@ -167,43 +167,66 @@
 // export default ConversationPage;
 "use client";
 
+import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
 import { useChat } from "ai/react";
+import { useTheme } from "next-themes";
+import { ElementRef, useEffect, useRef } from "react";
+import { BeatLoader } from "react-spinners";
 
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit, data } = useChat();
+  const { messages, input, handleInputChange, handleSubmit, data, isLoading } =
+    useChat();
+  const { theme } = useTheme();
+  const scrollRef = useRef<ElementRef<"div">>(null);
+  useEffect(() => {
+    scrollRef?.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages.length]);
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {/* {messages.length > 0
-        ? messages.map((m) => (
-            <div key={m.id} className="whitespace-pre-wrap">
-              {m.role === "user" ? "User: " : "AI: "}
-              {m.content}
-            </div>
-          ))
-        : null} */}
-      {messages.length > 0
-        ? messages.map((m) => (
-            <div
-              key={m.id}
-              className={cn("whitespace-pre-wrap", {
-                "text-blue-500 text-right p-4  gap-x-8 rounded-lg max-w-lg ":
-                  m.role === "user",
-                "text-green-500 p-4 w-full flex items-start gap-x-8 rounded-lg max-w-lg bg-muted":
-                  m.role !== "user",
-              })}
-            >
-              {m.role === "user" ? "" : "AI: "}
-              {m.content}
-            </div>
-          ))
-        : null}
-
+      {messages.length > 0 ? (
+        messages.map((m) => (
+          <div
+            key={m.id}
+            className={cn("whitespace-pre-wrap", {
+              "text-blue-500 text-right p-4  gap-x-8 rounded-lg max-w-lg ":
+                m.role === "user",
+              "text-green-500 p-4 w-full flex items-start gap-x-8 rounded-lg max-w-lg bg-muted":
+                m.role !== "user",
+            })}
+          >
+            {m.role === "user" ? "" : ""}
+            {m.content}
+          </div>
+        ))
+      ) : (
+        <>
+          <div className="flex flex-col items-center w-full  justify-center">
+            <p className="w-full">Ask a question to get started.</p>
+            <p className="w-full mt-3">
+              {" "}
+              Not custom trained so answer's might be inaccurate.
+            </p>
+            <p className="w-full mt-3">
+              The data for the answers have been fetch using Outerbase Commands.
+            </p>
+          </div>
+        </>
+      )}
       <form onSubmit={handleSubmit}>
-        <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+        {isLoading && (
+          <div className="p-4 rounded-lg w-1/2 flex items-center justify-center bg-muted mt-10">
+            <BeatLoader
+              color={theme === "light" ? "black" : "white"}
+              size={5}
+            />
+          </div>
+        )}
+        <div ref={scrollRef} />
+        <Input
+          className="fixed bottom-0 w-[80vw] md:w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
           value={input}
-          placeholder="Say something..."
+          placeholder="Talk to your data..."
           onChange={handleInputChange}
         />
       </form>
--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -26,7 +26,7 @@ export async function POST(req: Request) {
       // Replace this with actual properties of the book object
       return `Book ${index + 1}: ${book.name}, ${book.author}, ${
         book.createdAt
-      }, ${book.id}`;
+      }, Database id for book:${book.id}`;
     })
     .join(", ");
   console.log(bookDetails, "BOOK DETAIL");
@@ -51,27 +51,27 @@ export async function POST(req: Request) {
     })
     .join(", ");
   console.log(summaryDetails, "SUMMARY DETAIL");
-  const allBooks = await fetch(
-    `https://middle-indigo.cmd.outerbase.io/getAllBooks`,
-    {
-      method: "GET",
-      headers: {
-        "content-type": "application/json",
-      },
-    }
-  );
+  // const allBooks = await fetch(
+  //   `https://middle-indigo.cmd.outerbase.io/getAllBooks`,
+  //   {
+  //     method: "GET",
+  //     headers: {
+  //       "content-type": "application/json",
+  //     },
+  //   }
+  // );
 
-  const allbooks = await allBooks.json();
-  console.log(books, "INSIDE API");
-  const allbookDetails = allbooks.response.items
-    .map((book: any, index: any) => {
-      // Replace this with actual properties of the book object
-      return `Book ${index + 1}: ${book.name}, ${book.author}, ${
-        book.createdAt
-      }, ${book.id}`;
-    })
-    .join(", ");
-  console.log(allbookDetails, "ALL BOOK DETAIL");
+  // const allbooks = await allBooks.json();
+  // console.log(books, "INSIDE API");
+  // const allbookDetails = allbooks.response.items
+  //   .map((book: any, index: any) => {
+  //     // Replace this with actual properties of the book object
+  //     return `Book ${index + 1}: ${book.name}, ${book.author}, ${
+  //       book.createdAt
+  //     }, ${book.id}`;
+  //   })
+  //   .join(", ");
+  // console.log(allbookDetails, "ALL BOOK DETAIL");
   try {
     const { userId } = auth();
     const body = await req.json();
@@ -94,7 +94,8 @@ export async function POST(req: Request) {
     // Use the data from the endpoint in the messages
     messages.push({
       role: "system",
-      content: `As a data analyst, your task is to answer questions based on the books add by the user: ${bookDetails} and summaries: ${summaryDetails} submitted by the user. If the user asks about summaries, try to provide the corresponding book title from their submission. For reference, here is the list of all the books in the database: ${allbookDetails}.`,
+      // content: `As a data analyst, your task is to answer questions based on the books add by the user: ${bookDetails} and summaries: ${summaryDetails} submitted by the user. If the user asks about summaries, try to provide the corresponding book title from their submission. For reference, here is the list of all the books in the database: ${allbookDetails}.`,
+      content: `As a data analyst, your task is to answer questions based on the books add by the user: ${bookDetails} and summaries: ${summaryDetails} submitted by the user. If the user asks about summaries, try to provide the corresponding book title from their submission. `,
     });
     console.log(messages, "MESSAGES");
     const response = await openai.chat.completions.create({
