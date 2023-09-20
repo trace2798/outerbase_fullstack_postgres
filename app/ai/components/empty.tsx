7dd52bd0d981da2f534365edb18c60383031c9af
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-09-20 13:11:11 +0530

    improve in prompt

--- app/ai/components/empty.tsx
+++ app/ai/components/empty.tsx
@@ -1,7 +1,10 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { UseChatHelpers } from "ai/react";
 import { ArrowRight } from "lucide-react";
+import { useEffect, useState } from "react";
+import Typewriter from "typewriter-effect";
 
 const exampleMessages = [
   {
@@ -15,35 +18,54 @@ const exampleMessages = [
 ];
 
 export function EmptyStateAI({ setInput }: Pick<UseChatHelpers, "setInput">) {
+  const [isTypingDone, setIsTypingDone] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsTypingDone(true);
+    }, 11000);
+
+    return () => clearTimeout(timer); // This will clear the timeout if the component unmounts before the timeout finishes
+  }, []);
   return (
     <>
-      <Card
-        className="p-5 bg-muted
-          "
-      >
-        <div className="flex flex-col items-center w-full justify-center">
-          <p className="w-full mt-3">
-            {" "}
-            Not custom trained on this data so response likely to hallucinate.
-          </p>
-          <p className="w-full mt-3">
-            The data for the answers have been fetch using Outerbase Commands.
-          </p>
+      {!isTypingDone && (
+        <div className="text-2xl font-ranadeRegular">
+          <Typewriter
+            options={{
+              strings: ["Welcome", "Ask your data a question"],
+              autoStart: true,
+              loop: true,
+            }}
+          />
         </div>
-        <div className="mt-4 flex flex-col items-start space-y-2">
-          {exampleMessages.map((message, index) => (
-            <Button
-              key={index}
-              variant="link"
-              className="h-auto p-0 text-base"
-              onClick={() => setInput(message.message)}
-            >
-              <ArrowRight className="mr-2 text-muted-foreground" />
-              {message.heading}
-            </Button>
-          ))}
-        </div>
-      </Card>
+      )}
+      {isTypingDone && (
+        <Card className="p-5 bg-muted font-ranadeLight">
+          <div className="flex flex-col items-center w-full justify-center">
+            <p className="w-full mt-3">
+              {" "}
+              Not custom trained on this data so response likely to hallucinate.
+            </p>
+            <p className="w-full mt-3">
+              The data for the answers have been fetch using Outerbase Commands.
+            </p>
+          </div>
+          <div className="mt-4 flex flex-col items-start space-y-2">
+            {exampleMessages.map((message, index) => (
+              <Button
+                key={index}
+                variant="link"
+                className="h-auto p-0 text-base"
+                onClick={() => setInput(message.message)}
+              >
+                <ArrowRight className="mr-2 text-muted-foreground" />
+                {message.heading}
+              </Button>
+            ))}
+          </div>
+        </Card>
+      )}
     </>
   );
 }
--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -3,13 +3,109 @@ import { NextResponse } from "next/server";
 import OpenAI from "openai";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY || "",
+// });
+
+// export async function POST(req: Request) {
+//   const { userId } = auth();
+//   console.log("1 FIrst ONe");
+// const bookByUserId = await fetch(
+//   `https://middle-indigo.cmd.outerbase.io/getBooksByUserId?user_id=${userId}`,
+//   {
+//     method: "GET",
+//     headers: {
+//       "content-type": "application/json",
+//     },
+//   }
+// );
+
+//   const books = await bookByUserId.json();
+//   console.log(books, "INSIDE API");
+//   const bookDetails = books.response.items
+//     .map((book: any, index: any) => {
+//       return `Book ${index + 1}: ${book.name}, ${book.author}, ${
+//         book.createdAt
+//       }, Database id for book:${book.id}`;
+//     })
+//     .join(", ");
+//   console.log(bookDetails, "BOOK DETAIL");
+
+//   const reviewsByUserId = await fetch(
+//     `https://middle-indigo.cmd.outerbase.io/getSummariesByUserId?user_id=${userId}`,
+//     {
+//       method: "GET",
+//       headers: {
+//         "content-type": "application/json",
+//       },
+//     }
+//   );
+//   console.log(reviewsByUserId, "OUTERBASE COMMAND Review");
+//   const reviews = await reviewsByUserId.json();
+//   const reviewDetails = reviews.response.items
+//     .map((review: any, index: any) => {
+//       return `Review ${review.id}: ${review.title}, ${review.content}, Book database id for  on which the review is: ${review.book_id}, ${review.createdAt}`;
+//     })
+//     .join(", ");
+//   console.log(reviewDetails, "review DETAIL");
+
+// const allBooks = await fetch(
+//   `https://middle-indigo.cmd.outerbase.io/getAllBooks`,
+//   {
+//     method: "GET",
+//     headers: {
+//       "content-type": "application/json",
+//     },
+//   }
+// );
+
+//   const allbooks = await allBooks.json();
+//   console.log(books, "INSIDE API");
+//   const allbookDetails = allbooks.response.items
+//     .map((book: any, index: any) => {
+//       return `Book List from Database${book.id}:${book.name}, Book Database ID:${book.id}`;
+//     })
+//     .join(", ");
+//   console.log(allbookDetails, "ALL BOOK DETAIL");
+//   try {
+//     const { userId } = auth();
+//     const body = await req.json();
+//     const { messages } = body;
+
+//     if (!userId) {
+//       return new NextResponse("Unauthorized", { status: 401 });
+//     }
+
+//     if (!messages) {
+//       return new NextResponse("Messages are required", { status: 400 });
+//     }
+
+//     messages.push({
+//       role: "system",
+//       content: `As a data analyst named Fionaa, your task is to answer questions based on the books add by the user: ${bookDetails} and reviews: ${reviewDetails} submitted by the user. If the user asks about summaries, try to provide the corresponding book title from their submission.  Try to connect the review book_id with the id from: ${allbookDetails},`,
+//     });
+//     console.log(messages, "MESSAGES");
+//     const response = await openai.chat.completions.create({
+//       model: "gpt-3.5-turbo",
+//       messages,
+//       stream: true,
+//     });
+//     const stream = OpenAIStream(response);
+//     // return NextResponse.json(response.data.choices[0].message);
+//     return new StreamingTextResponse(stream);
+//   } catch (error) {
+//     console.log("[CONVERSATION_ERROR]", error);
+//     return new NextResponse("Internal Error", { status: 500 });
+//   }
+// }
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY || "",
 });
 
 export async function POST(req: Request) {
   const { userId } = auth();
-  console.log("1 FIrst ONe");
+
+  // Fetch all books and reviews by user ID as before
   const bookByUserId = await fetch(
     `https://middle-indigo.cmd.outerbase.io/getBooksByUserId?user_id=${userId}`,
     {
@@ -19,17 +115,7 @@ export async function POST(req: Request) {
       },
     }
   );
-
   const books = await bookByUserId.json();
-  console.log(books, "INSIDE API");
-  const bookDetails = books.response.items
-    .map((book: any, index: any) => {
-      return `Book ${index + 1}: ${book.name}, ${book.author}, ${
-        book.createdAt
-      }, Database id for book:${book.id}`;
-    })
-    .join(", ");
-  console.log(bookDetails, "BOOK DETAIL");
 
   const reviewsByUserId = await fetch(
     `https://middle-indigo.cmd.outerbase.io/getSummariesByUserId?user_id=${userId}`,
@@ -42,12 +128,8 @@ export async function POST(req: Request) {
   );
   console.log(reviewsByUserId, "OUTERBASE COMMAND Review");
   const reviews = await reviewsByUserId.json();
-  const reviewDetails = reviews.response.items
-    .map((review: any, index: any) => {
-      return `Review ${review.id}: ${review.title}, ${review.content}, Book database id for  on which the review is: ${review.book_id}, ${review.createdAt}`;
-    })
-    .join(", ");
-  console.log(reviewDetails, "review DETAIL");
+
+  // Fetch all books
 
   const allBooks = await fetch(
     `https://middle-indigo.cmd.outerbase.io/getAllBooks`,
@@ -58,15 +140,25 @@ export async function POST(req: Request) {
       },
     }
   );
-
   const allbooks = await allBooks.json();
-  console.log(books, "INSIDE API");
-  const allbookDetails = allbooks.response.items
-    .map((book: any, index: any) => {
-      return `Book List from Database${book.id}:${book.name}, Book Database ID:${book.id}`;
-    })
-    .join(", ");
-  console.log(allbookDetails, "ALL BOOK DETAIL");
+
+  // Create a new array to hold reviews with their associated books
+  let reviewsWithBooks = [];
+
+  // Loop through each review
+  for (let review of reviews.response.items) {
+    // Find the book for this review from the complete list of books
+    let bookForThisReview = allbooks.response.items.find(
+      (book: any) => book.id === review.book_id
+    );
+
+    // Add review and its associated book to the array
+    reviewsWithBooks.push({
+      review: `Review ${review.id}: ${review.title}, ${review.content}, Book database id for  on which the review is: ${review.book_id}, ${review.createdAt}`,
+      book: `Book: ${bookForThisReview.name}, ${bookForThisReview.author}, ${bookForThisReview.createdAt}, Database id for book:${bookForThisReview.id}`,
+    });
+  }
+
   try {
     const { userId } = auth();
     const body = await req.json();
@@ -80,18 +172,28 @@ export async function POST(req: Request) {
       return new NextResponse("Messages are required", { status: 400 });
     }
 
-    messages.push({
-      role: "system",
-      content: `As a data analyst named Fionaa, your task is to answer questions based on the books add by the user: ${bookDetails} and reviews: ${reviewDetails} submitted by the user. If the user asks about summaries, try to provide the corresponding book title from their submission.  Try to connect the review book_id with the id from: ${allbookDetails},`,
-    });
-    console.log(messages, "MESSAGES");
+    // Add each review with its associated book to the messages
+    for (let item of reviewsWithBooks) {
+      messages.push({
+        role: "system",
+        content: `As a data analyst named Fionaa, your task is to answer questions based on the following data: ${item.review} and its associated book: ${item.book}. TRY TO BE AS PRECISE as possible.`,
+      });
+    }
+    for (let book of books.response.items) {
+      messages.push({
+        role: "system",
+        content: `You have added the following book: Book: ${book.name}, ${book.author}, ${book.createdAt}, Database id for book:${book.id}`,
+      });
+    }
+
     const response = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
       messages,
       stream: true,
     });
+
     const stream = OpenAIStream(response);
-    // return NextResponse.json(response.data.choices[0].message);
+
     return new StreamingTextResponse(stream);
   } catch (error) {
     console.log("[CONVERSATION_ERROR]", error);
